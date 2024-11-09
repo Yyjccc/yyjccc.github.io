27c18024ecd18c764b89202dde46bf4ba2a283d7
yyj <yanyongjuyyj@outlook.com>
2024-11-09 23:56:15 +0800

    Site updated: 2024-11-09 23:56:10

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1730480331214";
+const VERSION = "1731167462557";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
