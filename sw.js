a6a2b95a4eee58e303b63ff8bb87ff41c083511b
yyj <yanyongjuyyj@outlook.com>
2025-01-27 15:38:46 +0800

    Site updated: 2025-01-27 15:34:55

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1731169629388";
+const VERSION = "1737963240774";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
